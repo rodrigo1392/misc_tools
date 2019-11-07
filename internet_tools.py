f54d2f8aef086a403ecb96e8f4d72aa6d7e6bfff
Rodrigo Rivero <rodrigo.m.rivero13@gmail.com>
2019-11-07 10:38:32 -0300

    Added internet tools module

---
+++ internet_tools.py
@@ -0,0 +1,18 @@
+""" Functions to be used by a Python 3 interpreter.
+    Developed by Rodrigo Rivero.
+    https://github.com/rodrigo1392"""
+
+
+def telegram_bot_send_text(bot_token, bot_chat_id, bot_message):
+    import requests
+    """
+    Sends telegram message through bot.
+    Inputs: bot_token. Token of telegram bot.
+            bot_chat_id. ID from chat.
+            bot_message. String to be sent.
+    Output: JSON variable with details of the message.
+    """
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + \
+                '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
