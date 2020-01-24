fbb18d9552cf10df9720936eb15e5f450cd1412d
rodrigo_1392 <rodrigo.m.rivero13@gmail.com>
2020-01-24 13:38:06 -0300

    Beginning process to include R packages for DOE process

---
+++ r_tools.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from rpy2.robjects.packages import importr
+from rpy2.robjects import r as R
+utils = importr('rsm')
+print(utils.__rdata__)
+print(type(R('pi')[0]))
+ChemReact = R['ChemReact']
+print(ChemReact)
+print(type(ChemReact))
+
+bbd = R['bbd']
+print(bbd(3,2)[5])
+
