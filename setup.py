58016e646a2828e84090f30c2b5e92a65d95d3aa
rodrigo_1392 <rodrigo.m.rivero13@gmail.com>
2020-02-23 10:59:16 -0300

    Pip minor change

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ root = Path(__file__).parent
 os.chdir(str(root))
 
 
-print(setuptools.find_packages())
-
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
@@ -25,14 +23,14 @@ with open("requirements.txt") as f:
     INSTALL_REQUIRES = [i.strip('\n') for i in f.readlines()]
 
 setuptools.setup(
-    name="misc_tools",
+    name="pymiscell",
     version="0.0.1",
     author="Rodrigo Rivero",
     author_email="rodrigo.m.rivero13@gmail.com",
     description="A package with miscellaneous, common use tools",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/rodrigo1392/misc_tools",
+    url="https://github.com/rodrigo1392/pymiscell",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
